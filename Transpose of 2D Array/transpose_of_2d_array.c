3d716a3cf4271c120241ba8517a7c6dc184a450f
SubhanRaj <shubhanraj2002@gmail.com>
2021-10-11 07:11:13 +0530

    C Program to transpose a 2D array

--- Transpose of 2D Array/transpose_of_2d_array.c
+++ Transpose of 2D Array/transpose_of_2d_array.c
@@ -1,34 +1,25 @@
-// C Program to find transpose of a matrix
+// C Program to transpose a 2D array
 #include <stdio.h>
-#define N 4
-
-// This function stores transpose of A[][] in B[][]
-void transpose(int A[][N], int B[][N])
-{
-    int i, j;
-    for (i = 0; i < N; i++)
-        for (j = 0; j < N; j++)
-            B[i][j] = A[j][i];
-}
 
 int main()
 {
-    int A[N][N] = {{1, 1, 1, 1},
-                   {2, 2, 2, 2},
-                   {3, 3, 3, 3},
-                   {4, 4, 4, 4}};
-
-    int B[N][N], i, j;
-
-    transpose(A, B);
-
-    printf("Result matrix is \n");
-    for (i = 0; i < N; i++)
+    int m, n, i, j, matrix[10][10], transpose[10][10]; // m = number of rows, n = number of columns, i, j = loop counters and matrix and transpose are 2D arrays
+    printf("Enter rows and columns :\n");              // Prompts user to enter rows and columns
+    scanf("%d%d", &m, &n);                             // Stores the number of rows and columns in m and n respectively
+    printf("Enter elements of the matrix\n");
+    for (i = 0; i < m; i++)             // Loops through the rows
+        for (j = 0; j < n; j++)         // Loops through the columns
+            scanf("%d", &matrix[i][j]); // Stores the elements of the matrix in matrix
+    // Transpose of the matrix
+    for (i = 0; i < m; i++)
+        for (j = 0; j < n; j++)
+            transpose[j][i] = matrix[i][j]; // Stores the transpose of the matrix in transpose
+    printf("Transpose of the matrix:\n");
+    for (i = 0; i < n; i++)
     {
-        for (j = 0; j < N; j++)
-            printf("%d ", B[i][j]);
+        for (j = 0; j < m; j++)
+            printf("%d\t", transpose[i][j]); // Prints the transpose of the matrix
         printf("\n");
     }
-
     return 0;
 }
